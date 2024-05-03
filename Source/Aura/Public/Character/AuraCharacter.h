83d858d195474f50537c14e8f28d371ed370e059
Sean-Sanii-Nejad <74361745+Sean-Sanii-Nejad@users.noreply.github.com>
2024-05-03 19:12:52 +0100

    Init Ability Actor Info

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -16,4 +16,10 @@ class AURA_API AAuraCharacter : public AAuraBaseCharacter
 	
 public:
 	AAuraCharacter();
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+
+private:
+	void InitAbilityActorInfo();
+
 };
