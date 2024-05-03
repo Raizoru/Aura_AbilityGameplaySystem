541d41f570e15069284e09c48296cf4f5d01c34d
Sean-Sanii-Nejad <74361745+Sean-Sanii-Nejad@users.noreply.github.com>
2024-05-03 08:24:33 +0100

    Created AuraPlayerController and BP Version

---
+++ Source/Aura/Private/MyClass.cpp
@@ -0,0 +1,12 @@
+// Sean Sanii Nejad
+
+
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ Source/Aura/Public/MyClass.h
@@ -0,0 +1,15 @@
+// Sean Sanii Nejad
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AURA_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
