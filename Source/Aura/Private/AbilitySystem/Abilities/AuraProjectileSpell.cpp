ae76e45968d78d4eb0ea08c997c83bc27ae6a6a1
Sean-Sanii-Nejad <74361745+Sean-Sanii-Nejad@users.noreply.github.com>
2024-05-11 14:17:45 +0100

    Spawn FireBolt from Event

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -14,7 +14,12 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 	const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+	
+}
+
+void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
@@ -39,35 +44,29 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 	}
 }
 
-//void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation)
+//ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+//if (CombatInterface)
 //{
-//	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
-//	if (!bIsServer) return;
+//	const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
+//	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+//	Rotation.Pitch = 0.f;
 //
-//	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
-//	if (CombatInterface)
-//	{
-//		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
-//		FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
-//		Rotation.Pitch = 0.f;
+//	FTransform SpawnTransform;
+//	SpawnTransform.SetLocation(SocketLocation);
+//	SpawnTransform.SetRotation(Rotation.Quaternion());
 //
-//		FTransform SpawnTransform;
-//		SpawnTransform.SetLocation(SocketLocation);
-//		SpawnTransform.SetRotation(Rotation.Quaternion());
+//	AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+//		ProjectileClass,
+//		SpawnTransform,
+//		GetOwningActorFromActorInfo(),
+//		Cast<APawn>(GetOwningActorFromActorInfo()),
+//		ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
 //
-//		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
-//			ProjectileClass,
-//			SpawnTransform,
-//			GetOwningActorFromActorInfo(),
-//			Cast<APawn>(GetOwningActorFromActorInfo()),
-//			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+//	//TODO: Give the Projectile a Gameplay Effect Spec for causing Damage.
 //
-//		//TODO: Give the Projectile a Gameplay Effect Spec for causing Damage.
+//	const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
+//	const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
+//	Projectile->DamageEffectSpecHandle = SpecHandle;
 //
-//		const UAbilitySystemComponent* SourceASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
-//		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
-//		Projectile->DamageEffectSpecHandle = SpecHandle;
-//
-//		Projectile->FinishSpawning(SpawnTransform);
-//	}
+//	Projectile->FinishSpawning(SpawnTransform);
 //}
