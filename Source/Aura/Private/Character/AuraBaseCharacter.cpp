4435ac11aa8da32e0cf19b905411db654013bf1b
Sean-Sanii-Nejad <74361745+Sean-Sanii-Nejad@users.noreply.github.com>
2024-05-24 14:11:14 +0100

    Passively Listening for Events

--- Source/Aura/Private/Character/AuraBaseCharacter.cpp
+++ Source/Aura/Private/Character/AuraBaseCharacter.cpp
@@ -160,6 +160,7 @@ void AAuraBaseCharacter::AddCharacterAbilities()
 	if (!HasAuthority()) return;
 
 	AuraASC->AddCharacterAbilities(StartupAbilities);
+	AuraASC->AddCharacterPassiveAbilities(StartupPassiveAbilities);
 }
 
 void AAuraBaseCharacter::Dissolve()
